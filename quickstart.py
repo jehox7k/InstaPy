a262674f3a883e28cd93cd44c84503fe8f9f4329
jehox7k <33389025+jehox7k@users.noreply.github.com>
2018-02-12 09:46:07 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
 
-insta_username = ''
+insta_username = 'romancesfoto'
 insta_password = ''
 
 # set headless_browser=True if you want to run InstaPy on a server
@@ -15,15 +15,34 @@ try:
                       multi_logs=True)
     session.login()
 
-    # settings
-    session.set_upper_follower_count(limit=2500)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
+    # set up all the settings
+    #session.set_upper_follower_count(limit=2500)
+    session.set_do_comment(True, percentage=80)
+    session.set_comments(['Excelente galeria', 'muy buenas tus publicaciones', 'Buenos tus posts', 'Geniales tus publicaciones'])
+    #session.set_dont_include(['friend1', 'friend2', 'friend3'])
+    #session.set_dont_like(['pizza', 'girl'])
 
-    # actions
-    session.like_by_tags(['natgeo'], amount=1)
+    #session.set_dont_unfollow_active_users(enabled=True, posts=6)
+    #session.unfollow_users(amount=100, onlyInstapyMethod = 'FIFO', sleep_delay=10 )
+
+    session.set_ignore_users(['taebom', 'fernandoanunez'])
+
+    # do the actual liking
+
+    #session.set_user_interact(amount=3, randomize=True, percentage=100, media='Photo')
+    session.like_by_tags(['chileangirl'], amount=100)
+
+    session.like_by_locations(['758392441/caracas/'], amount=100)
+    session.like_by_locations(['216143032/la-guaira-venezuela/'], amount=100)
+    session.like_by_locations(['215994674/tucacas-falcon-venezuela/'], amount=100)
+    session.like_by_locations(['213922485/morrocoy-falcon-venezuela/'], amount=100)
+    session.like_by_locations(['267435277113211/parque-nacional-el-avila-caracas-venezuela/'], amount=100)
+    session.like_by_locations(['26914683/santiago-chile/'], amount=100)
+    session.like_by_locations(['216178352/vina-del-mar-chile/'], amount=100)
+    session.like_by_locations(['427386520/santiago-metropolitan-region/'], amount=100)
+    session.like_by_locations(['1020867845/iglesia-de-dios-de-la-profecia/'], amount=100)
+    session.like_by_locations(['250235962/loncura-valparaiso-chile/'], amount=100)
+    session.like_by_locations(['222190475/maipu-chile/'], amount=100)
 
 finally:
     # end the bot session
